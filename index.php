1b91455ab119049cec4bb744118e95cc9557485e
Leandro Nonato <lenonato@hotmail.com>
2018-01-02 11:09:37 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -2,11 +2,30 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//CARREGA UM USUARIO
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(3);
 
-echo $root;
+//Carrega uma lista de usuarios.
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+
+//Carrega um usuario usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("nonato", "momo");
+
+echo $usuario;
+
 
 /*$sql = new Sql();
 
